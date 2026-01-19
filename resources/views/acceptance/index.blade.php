7de4f627c7631e4477dd5dd8bb3bad5d26875648
GianlucaJC <morescogianluca@gmail.com>
2026-01-19 11:35:57 +0100

    datatable IT

--- resources/views/acceptance/index.blade.php
+++ resources/views/acceptance/index.blade.php
@@ -156,7 +156,31 @@
     <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
     <script>
         $(document).ready(function() {
-            $('#acceptancesTable').DataTable();
+            $('#acceptancesTable').DataTable({
+                language: {
+                    "sEmptyTable":     "Nessun dato presente nella tabella",
+                    "sInfo":           "Vista da _START_ a _END_ di _TOTAL_ elementi",
+                    "sInfoEmpty":      "Vista da 0 a 0 di 0 elementi",
+                    "sInfoFiltered":   "(filtrati da _MAX_ elementi totali)",
+                    "sInfoPostFix":    "",
+                    "sInfoThousands":  ".",
+                    "sLengthMenu":     "Visualizza _MENU_ elementi",
+                    "sLoadingRecords": "Caricamento...",
+                    "sProcessing":     "Elaborazione...",
+                    "sSearch":         "Cerca:",
+                    "sZeroRecords":    "La ricerca non ha portato alcun risultato.",
+                    "oPaginate": {
+                        "sFirst":      "Inizio",
+                        "sPrevious":   "Precedente",
+                        "sNext":       "Successivo",
+                        "sLast":       "Fine"
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": attiva per ordinare la colonna in ordine crescente",
+                        "sSortDescending": ": attiva per ordinare la colonna in ordine decrescente"
+                    }
+                }
+            });
         });
     </script>
 </body>
